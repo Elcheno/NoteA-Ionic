f5688e5248cd15bae959bc2eff6caffa6f7b02ad
Elcheno <r.c.a2312@gmail.com>
2023-12-07 23:03:20 +0100

    fix update note

--- src/app/components/update-note/update-note.component.ts
+++ src/app/components/update-note/update-note.component.ts
@@ -4,13 +4,31 @@ import { IonicModule } from '@ionic/angular';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Note } from 'src/app/model/note';
 import { UIService } from 'src/app/services/ui.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Position } from 'src/app/model/position';
 
 @Component({
   selector: 'app-update-note',
   templateUrl: './update-note.component.html',
   styleUrls: ['./update-note.component.scss'],
   standalone: true,
-  imports: [ IonicModule, FormsModule, ReactiveFormsModule ]
+  imports: [ IonicModule, FormsModule, ReactiveFormsModule ],
+  animations: [
+    trigger('btnAnimate', [
+      state('success', style({
+        width: '180px'
+      })),
+      state('primary', style({
+        width: '100px'
+      })),
+      transition('primary => success', [
+        animate('.5s')
+      ]),
+      transition('success => primary', [
+        animate('.25s')
+      ])
+    ])
+  ]
 })
 export class UpdateNoteComponent implements OnInit {
 
@@ -21,12 +39,18 @@ export class UpdateNoteComponent implements OnInit {
   private uiService = inject(UIService);
   
   public form!: FormGroup;
+  public img: string = '';
+  public location: Position = {
+    latitude: '',
+    longitude: ''
+  };
 
   constructor() {
     this.form = this.formB.group({
       title:['',[Validators.required,Validators.minLength(4)]],
       description:['']
     });
+
   }
 
   ngOnInit() {
@@ -34,6 +58,9 @@ export class UpdateNoteComponent implements OnInit {
       const { key, date, img, position, ...aux } = this.note;
       this.form.setValue(aux);
     }
+
+    this.img = this.note.img;
+    this.location = this.note.position;
   }
 
   cancel() {
@@ -47,8 +74,8 @@ export class UpdateNoteComponent implements OnInit {
       title: this.form.value.title,
       description: this.form.value.description,
       date: this.note.date,
-      img: this.note.img,
-      position: this.note.position
+      img: this.img,
+      position: this.location
     };
 
     const resultDismiss = await this.uiService.dismissQuestion('Are you sure?');
@@ -61,7 +88,7 @@ export class UpdateNoteComponent implements OnInit {
   async removeLocation() {
     const resultDismiss = await this.uiService.dismissQuestion('Are you sure?');
     if (resultDismiss && resultDismiss === 'confirm') {
-      this.note.position = {
+      this.location = {
         latitude: '',
         longitude: ''
       }
@@ -71,7 +98,7 @@ export class UpdateNoteComponent implements OnInit {
   async removeImg() {
     const resultDismiss = await this.uiService.dismissQuestion('Are you sure?');
     if (resultDismiss && resultDismiss === 'confirm') {
-      this.note.img = '';
+      this.img = '';
     }
   }
 
